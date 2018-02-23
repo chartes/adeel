0bb458a167d7df223b60e63941d7f0afa6e68e0c
Pilla Julien <vrlitwhai.kridanik@gmail.com>
2018-02-23 10:54:53 +0100

    ajout du champ type de note & renommmage type_commentary, typ_acte

--- db_utils/db_import.py
+++ db_utils/db_import.py
@@ -25,10 +25,12 @@ helpers
 """
 get_manifest_url = lambda id: "http://193.48.42.68/adele/iiif/manifests/man{0}.json".format(id)
 get_image_id = lambda id: "http://193.48.42.68/loris/adele/dossiers/{0}.jpg/full/full/0/default.jpg".format(id)
-get_insert_stmt = lambda table,fields,values : "INSERT INTO {0} ({1}) VALUES ({2});".format(table, fields, values)
-get_delete_stmt = lambda table,where_clause="" : "DELETE FROM {0} {1};".format(table, where_clause)
+get_insert_stmt = lambda table,fields,values: "INSERT INTO {0} ({1}) VALUES ({2});".format(table, fields, values)
+get_delete_stmt = lambda table,where_clause="": "DELETE FROM {0} {1};".format(table, where_clause)
 
 clean = lambda s : s.replace("&amp;","&").replace("&gt;",">").replace("&lt;","<")
+nullIfNone = lambda v, callback=None : "null" if v is None else callback(v) if callback is not None else v
+escapeQuotes = lambda s : s.replace('"', '\\"')
 
 p = re.compile("<[^>]+>(.*)<\/[^>]+>")
 def get_tag_xml_content(node):
@@ -42,7 +44,6 @@ def get_tag_xml_content(node):
 #    parts = filter(None, parts)
 #    return ''.join(parts).strip()
 
-
 """
 inserts
 """
@@ -55,16 +56,12 @@ def insert_image_zone(dossier):
             dossier["img_id"],
             str(zone_id),
             t["coords"],
-            '"{0}"'.format(t["note"].replace('"', '\\"')) if t["note"] is not None else "null"]
-        )
+            nullIfNone(t["note"], lambda t: '"{0}"'.format(escapeQuotes(t)))
+        ])
         zone_id += 1
     #generate insert statements
-    stmts = [
-        get_insert_stmt(
-            "image_zone",
-            "manifest_url,img_id,zone_id,coords,note",
-            '"{0}","{1}",{2},"{3}",{4}'.format(*value)
-        )
+    stmts = [ get_insert_stmt("image_zone", "manifest_url,img_id,zone_id,coords,note",
+                              '"{0}","{1}",{2},"{3}",{4}'.format(*value))
         for value in values
     ]
     return stmts
@@ -108,7 +105,7 @@ for f in filenames:
     }
 
     """
-    table image_zone
+    tables image & image_zone
     """
     try:
         doc = ET.parse(os.path.join(ROOT,f))
@@ -122,18 +119,15 @@ for f in filenames:
     facsim_coord_figdesc = doc.xpath(XPATH_TI_FACSIM_COORDS_FIGDESC, namespaces={"ti":"http://www.tei-c.org/ns/1.0"})
     facsim_note_figdesc = doc.xpath(XPATH_TI_FACSIM_ANNO_FIGDESC, namespaces={"ti":"http://www.tei-c.org/ns/1.0"})
 
-    facsim_image_zone_transcription = zip(facsim_coord_tr, facsim_note_tr)
-    facsim_image_zone_figdesc = zip(facsim_coord_figdesc, facsim_note_figdesc)
-
     # Récupérations des zones de transcription
-    for coords_tr, note_tr in facsim_image_zone_transcription:
+    for coords_tr, note_tr in zip(facsim_coord_tr, facsim_note_tr):
         try:
             check_coords_validity(coords_tr)
             dossiers[f]["image_zone"].append({"coords": coords_tr, "note": None})
         except ValueError:
             facsim_coords_error.append((f, coords_tr))
     # Récupération des zones d'annotations & l'annotation associée
-    for coords_figdesc, note_figdesc in facsim_image_zone_figdesc:
+    for coords_figdesc, note_figdesc in zip(facsim_coord_figdesc, facsim_note_figdesc):
         try:
             check_coords_validity(coords_figdesc)
             dossiers[f]["image_zone"].append({"coords": coords_figdesc, "note": clean(get_tag_xml_content(note_figdesc))})
@@ -141,8 +135,9 @@ for f in filenames:
             facsim_coords_error.append((f, coords_figdesc))
 
     """
-    table XXX
+    tables transcription & transcriptionHasNote
     """
+
 
 
 """
@@ -152,17 +147,6 @@ print("=" * 80)
 print("facsim coords error:")
 pprint.pprint(set(facsim_coords_error))
 print("file parsing error (nb files): {0}".format(cnt_file_parsing_error))
-
-#print("=" * 80)
-#pprint.pprint(dossiers["1.xml"])
-
-#print("=" * 80)
-#cnt = 0
-#for dossier in dossiers.values():
-#    for i in insert_image_zone(dossier):
-#        #print(i)
-#        cnt+=#1
-#print("NB_INSERT image_zone:", cnt)
 
 
 add_sql_comment = lambda f, c="=" : f.write("--" + c * 40 + "\n")
@@ -186,9 +170,5 @@ with open('insert_statements.sql', 'w+') as f:
         for i in insert_image_zone(dossier):
             f.write(i + "\n")
         add_sql_comment(f, '=')
-
-
-#for d in insert_image_zone(dossiers["20.xml"]):
-#    print(d)
 
 
